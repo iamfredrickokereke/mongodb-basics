b26e86f9e638493bdcc5504d6e61ed35f12fabc5
Fredrick Kelly <38326256+iamfredrickokereke@users.noreply.github.com>
2020-04-16 23:01:09 +0100

    Update interns.js

--- interns.js
+++ interns.js
@@ -1,35 +1,24 @@
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/";
+let movieList =
+ [
+  {movie: "The Banker", year: "2020", rating: 8},
+  {movie: "Bad Boys", year: "2020", rating: 7}, 
+  {movie: "The Hunt", year: "2020", rating: 7}, 
+  {movie: "Bloodshot", year: "2020", rating: 7.5}, 
+  {movie: "First Cow", year: "2020", rating: 6.5}
+ ];
 
-// import the assert module for checks
 
-const assert = require('assert');
+const collections = function(err, db) {
+  if (err) throw err;
+  let dbo = db.db("moviesdb");
+  let movieCollections = dbo.collection("myMovies")
+  movieCollections.insertMany(movieList, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+};
 
-// create the function to export
-
-const theMovies = (db, callback) => {
-
-    //creating the myMovies collection
-
-    let myMovies = db.collection("movies");
-
-    //available Documents
-    let movies = 
-
-    [ 
-      {movie: "The Banker", year: "2020", rating: 8},  
-      {movie: "Bad Boys", year: "2020", rating: 7}, 
-      {movie: "The Hunt", year: "2020", rating: 7}, 
-      {movie: "Bloodshot", year: "2020", rating: 7.5},
-      {movie: "First Cow", year: "2020", rating: 6.5} 
-    ]
-
-    //insert movies into the collection and checking for error
-    myMovies.insertMany(movies, function(err, result){
-    assert.equal(err, null);
-    assert.equal(5, result.result.n);
-    assert.equal(5, result.ops.length);
-    console.log("The total number of Movies inserted: "+ result.insertedCount);
-    callback(result);
-    });
- };
-
-module.exports = theMovies;
+MongoClient.connect(url, collections);
