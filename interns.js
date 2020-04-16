13a67dc00bbe8b89a3f182a2fc0736f47e64dcd6
Fredrick Kelly <38326256+iamfredrickokereke@users.noreply.github.com>
2020-04-16 23:53:06 +0100

    Update interns.js

--- interns.js
+++ interns.js
@@ -1,13 +1,14 @@
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/";
-let movieList =
- [
+const { MongoClient } = require('mongodb');
+
+const  url = "mongodb://localhost:27017/";
+
+let movieList = [
   {movie: "The Banker", year: "2020", rating: 8},
   {movie: "Bad Boys", year: "2020", rating: 7}, 
   {movie: "The Hunt", year: "2020", rating: 7}, 
   {movie: "Bloodshot", year: "2020", rating: 7.5}, 
   {movie: "First Cow", year: "2020", rating: 6.5}
- ];
+];
 
 
 const collections = function(err, db) {
@@ -21,4 +22,4 @@ const collections = function(err, db) {
   });
 };
 
-MongoClient.connect(url, collections);
+MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, collections);
